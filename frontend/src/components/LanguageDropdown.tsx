12360e05a2e8bf62db894f3afc2af92733420cd2
florian <florian.guiramand@protonmail.com>
2026-04-03 15:47:23 +0200

    finish navbar

--- frontend/src/components/LanguageDropdown.tsx
+++ frontend/src/components/LanguageDropdown.tsx
@@ -1,9 +1,30 @@
 import styles from "./LanguageDropdown.module.css";
+import React, {useState} from "react";
 
-export default function LanguageDropdown() {
-    return (<div className={styles.dropdown}>
-            <button className="clean-btn">English</button>
-            <button className="clean-btn">French</button>
-            <button className="clean-btn">Spanish</button>
-    </div>)
+export default function LanguageDropdown(Icon: React.JSX.Element, className: string) {
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedLanguage, setSelectedLanguage] = useState([true, false, false]);
+    const languages = ["English", "French", "Spanish"];
+
+    const handleSwitchLanguage = (index: number) => {
+        const newLanguage = [false, false, false];
+        newLanguage[index] = true;
+        setSelectedLanguage(newLanguage);
+        setIsOpen(false);
+    }
+
+    return (
+        <div
+            className={className}
+            onMouseEnter={() => (setIsOpen(true))}
+            onMouseLeave={() => (setIsOpen(false))}>
+
+            {Icon}
+            {isOpen && <div className={styles.dropdown + " border"}>
+                {languages.map((language, i) => (
+                    <button key={language} className={"underline clean-btn hairline" + (selectedLanguage[i] ? " selected" : "")} onClick={() => handleSwitchLanguage(i)}>{language}</button>
+                ))}
+            </div>}
+        </div>
+    );
 }

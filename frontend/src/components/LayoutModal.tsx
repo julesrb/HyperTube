8e5cde35fe292b702a0d070b40b358a8b8994087
florian <florian.guiramand@protonmail.com>
2026-04-03 19:52:13 +0200

    feat: implement styled modal layout with close button and context integration

--- frontend/src/components/LayoutModal.tsx
+++ frontend/src/components/LayoutModal.tsx
@@ -1,32 +1,21 @@
 "use client";
 
+import styles from "@/styles/LayoutModal.module.css";
+import React from "react";
+import Image from "next/image";
+
 export default function ModalLayout({ children, onClose, }: { children: React.ReactNode; onClose: () => void; }) {
     return (
-        <div
-            onClick={onClose}
-            style={{
-                position: "fixed",
-                inset: 0,
-                background:
-                    "rgba(0,0,0,0.6)",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                zIndex: 1000,
-            }}
-        >
-            <div
-                onClick={(e) =>
-                    e.stopPropagation()
-                }
-                style={{
-                    background: "white",
-                    padding: "2rem",
-                    borderRadius: "8px",
-                    width: "300px",
-                }}
-            >
-                {children}
+        <div onClick={onClose} className={styles.bg} >
+            <div onClick={(e) => e.stopPropagation()} className={styles.modal + " border"}>
+                <div className={styles.header}>
+                    <button className="clean-btn" onClick={onClose}>
+                        <Image className="black-cross" src="/icons/cross.svg" alt="cross" width={30} height={30}/>
+                    </button>
+                </div>
+                <div className={styles.content}>
+                    {children}
+                </div>
             </div>
         </div>
     );

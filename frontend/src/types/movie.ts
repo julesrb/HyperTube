89cd5e5c7951022a94b221f016d7af2194577ce4
florian <florian.guiramand@protonmail.com>
2026-04-08 19:43:10 +0200

    feat: add MovieSmallCard component and movie type definition for movie display

---
+++ frontend/src/types/movie.ts
@@ -0,0 +1,16 @@
+export type tmovies = {
+    id: number
+    title: string
+    src: string
+    year: string
+};
+
+export const movies : tmovies[] = [
+    {id: 1, title: "Avatar: Fire and Ash", src: "dVv28yL7kyeMK3sUTWVSKrZC6tV.jpg", year: "2025"},
+    {id: 2, title: "The Drama", src: "heV850U3uf2FNxLW8QPcKyZMfdx.jpg", year: "2026"},
+    {id: 3, title: "Project Hail Mary", src: "kutdTXxqWJ7KYmnll8nSrtWWsCR.jpg", year: "2026"},
+    {id: 4, title: "The Super Mario Galaxy Movie", src: "6RmUnKyBfLu6MfC7sya2WXjyVut.jpg", year: "2026"},
+    {id: 5, title: "Hoppers", src: "p5G1Fbn6hjQprNijkcjarf8FZWG.jpg", year: "2026"},
+    {id: 6, title: "The Bride!", src: "g7twdxxP2QuWLE8hOgSl5570WoX.jpg", year: "2026"},
+    {id: 7, title: "Send Help", src: "iXW9WMVOjhJ3wwOGz5Da42tU4wk.jpg", year: "2026"},
+];

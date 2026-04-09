04a9058ee1cd7bc639f4b8c7bfd8659c53e71916
florian <florian.guiramand@protonmail.com>
2026-04-09 17:28:16 +0200

    feat: add EyeIcon component and implement password visibility toggle in Input

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,15 +1,22 @@
 import React from "react";
+import EyeIcon from "@/components/icon/EyeIcon";
 
 export default function Input({type, placeholder, className }: {type: string, placeholder: string, className?: string}) {
     const isPassword = type === "password";
+    const [isPasswordVisible, setIsPasswordVisible] = React.useState(false);
 
-    return (<div className="flex flex-col w-full h-16">
-        {isPassword ?? <a href="#">Forgotten?</a>}
-        <input id={type} type={type} placeholder=""
-               className={"peer py-6 m-0 w-full h-8 bg-white text-black border-b focus:border-b-2 "  + className}/>
+    const handleTogglePasswordVisibility = () => {
+        setIsPasswordVisible(!isPasswordVisible);
+    }
+
+    return (<div className={"flex flex-col w-full h-16 relative " + className}>
+        <input id={type} type={isPasswordVisible && isPassword ? "text" : type} placeholder=""
+               className="peer py-4 m-0 w-full h-8 bg-white text-black border-b focus:border-b-2"/>
         <label htmlFor={type}
-               className="uppercase relative text-xs font-light bottom-14
-               peer-focus:text-xs peer-focus:font-light peer-focus:bottom-14
+               className="uppercase absolute text-xs font-hairline bottom-15
+               peer-focus:text-xs peer-focus:font-hairline peer-focus:bottom-15
                peer-placeholder-shown:font-normal peer-placeholder-shown:font-bold peer-placeholder-shown:bottom-9 peer-placeholder-shown:text-xl">{placeholder}</label>
-    </div>);
+        {isPassword && (<button className="absolute right-0 top-1" onClick={handleTogglePasswordVisibility}><EyeIcon crossed={isPasswordVisible}/></button>)}
+    </div>
+    );
 }

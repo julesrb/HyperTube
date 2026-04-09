3dcd5590b02a75508da96a3232b6539c12c36448
florian <florian.guiramand@protonmail.com>
2026-04-09 15:41:16 +0200

    feat: implement tabbed navigation for user profile, authentication, and movie history

--- frontend/src/app/users/page.tsx
+++ frontend/src/app/users/page.tsx
@@ -1,54 +1,88 @@
+"use client";
+
 import Input from "@/components/Input";
-import React from "react";
+import React, {useState} from "react";
 import Button from "@/components/Button";
 import DefaultUserIcon from "@/components/icon/DefaultUserIcon";
 import FilmSmallCard from "@/components/MovieSmallCard";
 import {movies} from "@/types/movie";
 
 export default function Page() {
+    const tabs = {profile: ProfileTab, auth: AuthTab, history: MovieHistoryTab};
+    const [activeTab, setActiveTab] = useState<keyof typeof tabs>("profile");
+    const ActiveTab = tabs[activeTab];
 
-    return (<div>
-            <div className="container mx-auto">
-                <h3>Profile</h3>
-                <div className="grid grid-cols-2">
-                    <div className="flex flex-col gap-4 items-start max-w-2/3 mx-auto">
-                        <Input type="email" placeholder="Email"></Input>
+    const switchTab = (tabName: string) => {
+        if (activeTab !== tabName) {
+            setActiveTab(tabName);
+        }
+    }
 
-                        <div className="flex gap-2 w-full">
-                            <Input type="firstname" placeholder="Firstname"></Input>
-                            <Input type="lastname" placeholder="Lastname"></Input>
-                        </div>
+    return (<div className="px-4">
+        <div className="flex h-16 mb-10">
+            <div className="border-b border-r w-12"></div>
+            {Object.keys(tabs).map((tabName, index) => (<button
+                key={index}
+                className={"uppercase font-condensed text-[41px] border-t border-r px-16 border-b" + (activeTab === tabName ? " border-b-white" : "")}
+                onClick={() => switchTab(tabName)}>{tabName}</button>))}
+            <div className="border-b w-full"></div>
+        </div>
+        <div>
+            <ActiveTab/>
+        </div>
 
-                        <Input type="username" placeholder="Username" className={"max-w-2/3"}></Input>
+    </div>);
+}
 
-                        <Button className="h-8">Save Changes</Button>
-                    </div>
 
-                    <div className="flex flex-col gap-2 items-center justify-center">
-                        <DefaultUserIcon className="mb-6"/>
-                        <Button className="h-8">Select New avatar</Button>
+function ProfileTab() {
+    return (<div className="container mx-auto">
+        <h3>Profile</h3>
+        <div className="grid grid-cols-2">
+            <div className="flex flex-col gap-4 items-start max-w-2/3 mx-auto">
+                <Input type="email" placeholder="Email"></Input>
 
-                        <button
-                            className={(true ? "text-red  custom-underline-red" : "text-gray") + " text-sm font-light"}>Remove
-                        </button>
-                    </div>
+                <div className="flex gap-2 w-full">
+                    <Input type="firstname" placeholder="Firstname"></Input>
+                    <Input type="lastname" placeholder="Lastname"></Input>
                 </div>
-            </div>
-            <div className="container mx-auto">
-                <h3>Password</h3>
 
-                <Input type="password" placeholder="Current password"></Input>
-                <Input type="password" placeholder="New password"></Input>
-                <Input type="password" placeholder="Confirm new password"></Input>
+                <Input type="username" placeholder="Username" className={"max-w-2/3"}></Input>
 
-                <Button className="h-8">Change</Button>
+                <Button className="h-8">Save Changes</Button>
             </div>
-            <div className="container mx-auto">
-                <h3>Films Watched</h3>
-                <div className="grid grid-cols-4">
-                    {movies.map((movie, index) => (
-                        <FilmSmallCard key={index} movie={movie}/>))}
-                </div>
+
+            <div className="flex flex-col gap-2 items-center justify-center">
+                <DefaultUserIcon className="mb-6"/>
+                <Button className="h-8">Select New avatar</Button>
+
+                <button
+                    className={(true ? "text-red  custom-underline-red" : "text-gray") + " text-sm font-light"}>Remove
+                </button>
             </div>
-        </div>);
+        </div>
+    </div>);
+}
+
+
+function AuthTab() {
+    return (<div className="container mx-auto">
+        <h3>Password</h3>
+
+        <Input type="password" placeholder="Current password"></Input>
+        <Input type="password" placeholder="New password"></Input>
+        <Input type="password" placeholder="Confirm new password"></Input>
+
+        <Button className="h-8">Change</Button>
+    </div>);
+}
+
+
+function MovieHistoryTab() {
+    return (<div className="container mx-auto">
+        <h3>Films Watched</h3>
+        <div className="grid grid-cols-4">
+            {movies.map((movie, index) => (<FilmSmallCard key={index} movie={movie}/>))}
+        </div>
+    </div>);
 }

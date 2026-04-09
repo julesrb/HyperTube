1eae899bd1ee6c9e9f20b1b71763676942a136ab
florian <florian.guiramand@protonmail.com>
2026-04-09 17:00:31 +0200

    feat: enhance input component with improved styling and label behavior

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -3,9 +3,13 @@ import React from "react";
 export default function Input({type, placeholder, className }: {type: string, placeholder: string, className?: string}) {
     const isPassword = type === "password";
 
-    return (<div className="flex flex-col w-full">
-        <label htmlFor={type} className="text-gray ml-1 font-light">{placeholder}</label>
+    return (<div className="flex flex-col w-full h-16">
         {isPassword ?? <a href="#">Forgotten?</a>}
-        <input id={type} type={type} placeholder="" className={"rounded-xs p-2 m-0 w-full h-8 bg-white text-black border "  + className}/>
+        <input id={type} type={type} placeholder=""
+               className={"peer py-6 m-0 w-full h-8 bg-white text-black border-b focus:border-b-2 "  + className}/>
+        <label htmlFor={type}
+               className="uppercase relative text-xs font-light bottom-14
+               peer-focus:text-xs peer-focus:font-light peer-focus:bottom-14
+               peer-placeholder-shown:font-normal peer-placeholder-shown:font-bold peer-placeholder-shown:bottom-9 peer-placeholder-shown:text-xl">{placeholder}</label>
     </div>);
 }

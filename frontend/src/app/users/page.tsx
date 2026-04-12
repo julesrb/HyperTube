8e6855630765dd589be34d1800a49ef36406ee10
florian <florian.guiramand@protonmail.com>
2026-04-12 18:20:39 +0200

    feat: enhance profile page layout and add avatar tab for improved user experience

--- frontend/src/app/users/page.tsx
+++ frontend/src/app/users/page.tsx
@@ -4,11 +4,11 @@ import Input from "@/components/Input";
 import React, {useState} from "react";
 import Button from "@/components/Button";
 import DefaultUserIcon from "@/components/icon/DefaultUserIcon";
-import FilmSmallCard from "@/components/MovieSmallCard";
 import {movies} from "@/types/movie";
+import MovieCard from "@/components/MovieCard";
 
 export default function Page() {
-    const tabs = {profile: ProfileTab, auth: AuthTab, history: MovieHistoryTab};
+    const tabs = {profile: ProfileTab, avatar: AvatarTab, auth: AuthTab, history: MovieHistoryTab};
     const [activeTab, setActiveTab] = useState<keyof typeof tabs>("profile");
     const ActiveTab = tabs[activeTab];
 
@@ -20,11 +20,11 @@ export default function Page() {
 
     return (<div className="px-4">
         <div className="flex items-center gap-4 justify-center mt-10 mb-16">
-            <span className="rounded-full bg-purple text-black size-22 flex items-center justify-center text-5xl font-condensed border">
+            <h3 className="font-bold rounded-full bg-purple size-22 flex items-center justify-center border">
                 FG
-            </span>
+            </h3>
             <div className="flex flex-col items-start">
-                <span className="text-6xl font-condensed uppercase tracking-wide">Florian G.</span>
+                <h2>Florian G.</h2>
                 <p className="uppercase">Member since 10.04.2026</p>
             </div>
         </div>
@@ -32,8 +32,8 @@ export default function Page() {
             <div className="border-b border-r w-12"></div>
             {(Object.keys(tabs) as Array<keyof typeof tabs>).map((tabName, index) => (<button
                 key={index}
-                className={"uppercase font-condensed tracking-wide text-4xl border-t border-r px-16 border-b" + (activeTab === tabName ? " border-b-white" : "")}
-                onClick={() => switchTab(tabName)}>{tabName}</button>))}
+                className={"border-t border-r px-16 border-b" + (activeTab === tabName ? " border-b-white" : "")}
+                onClick={() => switchTab(tabName)}><h4>{tabName}</h4></button>))}
             <div className="border-b w-full"></div>
         </div>
         <div>
@@ -45,53 +45,48 @@ export default function Page() {
 
 
 function ProfileTab() {
-    return (<div className="container mx-auto">
-        <h3>Profile</h3>
-        <div className="grid grid-cols-2">
-            <div className="flex flex-col gap-6 items-start max-w-2/3 mx-auto">
-                <Input type="email" placeholder="Email"></Input>
+    return (<div className="flex flex-col gap-4 items-start max-w-2/5 mx-auto">
+        <Input type="email" placeholder="Email"></Input>
 
-                <div className="flex gap-2 w-full">
-                    <Input type="firstname" placeholder="Firstname"></Input>
-                    <Input type="lastname" placeholder="Lastname"></Input>
-                </div>
-
-                <Input type="username" placeholder="Username" className={"max-w-2/3"}></Input>
-
-                <Button className="h-8">Save Changes</Button>
-            </div>
-
-            <div className="flex flex-col gap-2 items-center justify-center">
-                <DefaultUserIcon className="mb-6"/>
-                <Button className="h-8">Select New avatar</Button>
-
-                <button
-                    className={(true ? "text-red  custom-h-underline-red" : "text-gray") + " text-sm font-sans"}>Remove
-                </button>
-            </div>
+        <div className="flex gap-2 w-full">
+            <Input type="firstname" placeholder="Firstname"></Input>
+            <Input type="lastname" placeholder="Lastname"></Input>
         </div>
+
+        <Input type="username" placeholder="Username" className={"max-w-3/5"}></Input>
+
+        <Button className="h-8">Save Changes</Button>
     </div>);
 }
 
 
+function AvatarTab() {
+    return (<div className="flex flex-col gap-2 items-center justify-center">
+        <DefaultUserIcon className="mb-6"/>
+        <Button>Select New avatar</Button>
+
+        <button
+            className={(true ? "text-red  custom-h-underline-red" : "text-gray") + " text-sm font-sans"}>Remove
+        </button>
+    </div>);
+}
+
+
+
 function AuthTab() {
-    return (<div className="container mx-auto">
-        <h3>Password</h3>
+    return (<div className="max-w-2/5 mx-auto flex flex-col items-start gap-4">
 
         <Input type="password" placeholder="Current password"></Input>
         <Input type="password" placeholder="New password"></Input>
         <Input type="password" placeholder="Confirm new password"></Input>
 
-        <Button className="h-8">Change</Button>
+        <Button>Change</Button>
     </div>);
 }
 
 
 function MovieHistoryTab() {
-    return (<div className="container mx-auto">
-        <h3>Films Watched</h3>
-        <div className="grid grid-cols-4">
-            {movies.map((movie, index) => (<FilmSmallCard key={index} movie={movie}/>))}
-        </div>
+    return (<div className="grid grid-cols-3 gap-4">
+        {movies.map((movie, index) => (<MovieCard key={index} movie={movie}/>))}
     </div>);
 }

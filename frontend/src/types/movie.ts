819ec2a0aeef216c3f3eb5971c7a466405caf280
florian <florian.guiramand@protonmail.com>
2026-04-12 18:21:25 +0200

    feat: add backdrops to movie entries for enhanced detail

--- frontend/src/types/movie.ts
+++ frontend/src/types/movie.ts
@@ -3,14 +3,24 @@ export type tmovies = {
     title: string
     src: string
     year: string
+    backdrops: string[]
 };
 
 export const movies : tmovies[] = [
-    {id: 1, title: "Avatar: Fire and Ash", src: "dVv28yL7kyeMK3sUTWVSKrZC6tV.jpg", year: "2025"},
-    {id: 2, title: "The Drama", src: "heV850U3uf2FNxLW8QPcKyZMfdx.jpg", year: "2026"},
-    {id: 3, title: "Project Hail Mary", src: "kutdTXxqWJ7KYmnll8nSrtWWsCR.jpg", year: "2026"},
-    {id: 4, title: "The Super Mario Galaxy Movie", src: "6RmUnKyBfLu6MfC7sya2WXjyVut.jpg", year: "2026"},
-    {id: 5, title: "Hoppers", src: "p5G1Fbn6hjQprNijkcjarf8FZWG.jpg", year: "2026"},
-    {id: 6, title: "The Bride!", src: "g7twdxxP2QuWLE8hOgSl5570WoX.jpg", year: "2026"},
-    {id: 7, title: "Send Help", src: "iXW9WMVOjhJ3wwOGz5Da42tU4wk.jpg", year: "2026"},
+    {id: 1, title: "Avatar: Fire and Ash", src: "dVv28yL7kyeMK3sUTWVSKrZC6tV.jpg", year: "2025",
+        backdrops: ["3Dqievkc7krcTtDE2hjRkIsEzB1.webp", "iN41Ccw4DctL8npfmYg1j5Tr1eb.webp", "sdZSjtGUTSN8B3al5o0f2WoQfQQ.webp", "vm4H1DivjQoNIm0Vs6i3CTzFxQ0.webp"]},
+    {id: 2, title: "The Drama", src: "heV850U3uf2FNxLW8QPcKyZMfdx.jpg", year: "2026",
+        backdrops: ["1oKLEA9JOhvaBwLpqjROisvWMy7.webp", "gQxnJoswBzsENExzygVnFiemvK7.webp", "rw1WmxUvmbwRsbAX12m6toIg04j.webp", "t1S7cLojbI92zEkdTgkWnytW7Oa.webp", "vc7TeZyKhBeVA4YI3z1KAeIXqGV.webp", "yLjh1d3SlneSRVHz0ZrX1fuXSob.webp"]},
+    {id: 3, title: "Project Hail Mary", src: "kutdTXxqWJ7KYmnll8nSrtWWsCR.jpg", year: "2026",
+        backdrops: ["2I1OFQJ0L9T0dpU6FobKFWV2PxX.webp", "8Tfys3mDZVp4tNoH2ktm06a0Tau.webp"]},
+    {id: 4, title: "The Super Mario Galaxy Movie", src: "6RmUnKyBfLu6MfC7sya2WXjyVut.jpg", year: "2026",
+        backdrops: ["9Z2uDYXqJrlmePznQQJhL6d92Rq.webp", "kxQiIJ4gVcD3K6o14MJ72p5yRcE.webp", "xTd74mchq96pqfdlgmKDMQx12gv.webp"]},
+    {id: 5, title: "Hoppers", src: "p5G1Fbn6hjQprNijkcjarf8FZWG.jpg", year: "2026",
+        backdrops: ["2RrLuIfIzGWWIH8IAEo6o0IYHmx.webp", "7Zk07DUBunUp9E1LtLK63jJGiDk.webp", "gTKBGnxWVVg3yHA3mjJjnJRgU1v.webp", "u53UYu5XG2hNgWGvs3xGhAVzypl.webp"]},
+    {id: 6, title: "The Bride!", src: "g7twdxxP2QuWLE8hOgSl5570WoX.jpg", year: "2026",
+        backdrops: ["7LivZ6bgoG7JQ6c4wJCQmyjj6ye.webp", "l8rKKMU2M9dDULO9CEtDNdWAEUJ.webp", "uXgWbwxmCW6XZ3ylRThk9Sqhwwq.webp"]},
+    {id: 7, title: "Send Help", src: "iXW9WMVOjhJ3wwOGz5Da42tU4wk.jpg", year: "2026",
+        backdrops: ["8zeiIiI71I93ZnSTkda3WfrLxki.webp", "493MuzSs6iLyKM7aqsA30eODfsV.webp", "gCmfeKmEAZBP5gcXpiqb0gii9rS.webp", "hO2jx1H3XafR7Y8QbFgVH1sHTY9.webp"]},
+    {id: 8, title: "GOAT", src: "", year: "2026",
+        backdrops: ["6qMyijXWuDfrN8bXUJT1DErjQog.webp", "imkZLxabgZBdsWPnkPkFCHC9NCw.webp", "kEr4SY8y4ZS8drASBLzLGbQ0Zkm.webp", "tq3h43fZy0H80vzf47MAY7R9Mxo.webp", "ucDMmWAYOiBhd6cZCDWVK9DIIQ4.webp"]},
 ];

2ea0ea87e65cc771f24807526e8c208e6e361e6f
florian <florian.guiramand@protonmail.com>
2026-04-12 18:20:51 +0200

    feat: update Button component styles for improved height and padding

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Button({children, className}: {children: React.ReactNode, className?: string}) {
-    return (<button className={"text-nowrap rounded-xs uppercase px-4 bg-purple text-white cursor-pointer border border-black custom-btn " + className}>{children}</button>);
+    return (<button className={"text-nowrap uppercase px-5 h-10 bg-purple text-white cursor-pointer border border-black custom-btn " + className}>{children}</button>);
 }

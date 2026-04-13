8dac15bde8e3af09320dc3eb37cdb8df5590778c
florian <florian.guiramand@protonmail.com>
2026-04-13 10:22:41 +0200

    feat: update ModalLayout component to improve z-index and background styling

--- frontend/src/components/LayoutModal.tsx
+++ frontend/src/components/LayoutModal.tsx
@@ -4,9 +4,9 @@ import React from "react";
 
 export default function ModalLayout({ children, onClose, defaultLayout = true } : { children: React.ReactNode, onClose: () => void, defaultLayout?: boolean }) {
     if (!defaultLayout)
-        return (<div onClick={onClose} className="fixed inset-0 z-10" >{children}</div>);
+        return (<div onClick={onClose} className="fixed inset-0 z-30" >{children}</div>);
     return (
-        <div onClick={onClose} className="fixed bg-gray inset-0 flex justify-center items-center z-10">
+        <div onClick={onClose} className="fixed inset-0 flex justify-center items-center z-30 bg-black/50">
             <div onClick={(e) => e.stopPropagation()} className="p-6 bg-white custom-shadow-m border w-90">
                 <div className="flex flex-col gap-2 items-start">
                     {children}

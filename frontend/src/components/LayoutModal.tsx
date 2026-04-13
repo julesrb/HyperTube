513acec6a68d94574e56e168c83dc26c5bd669b3
florian <florian.guiramand@protonmail.com>
2026-04-13 19:12:39 +0200

    feat: update z-index values for LanguageDropdown and LayoutModal components to improve layering

--- frontend/src/components/LayoutModal.tsx
+++ frontend/src/components/LayoutModal.tsx
@@ -4,9 +4,9 @@ import React from "react";
 
 export default function ModalLayout({ children, onClose, defaultLayout = true } : { children: React.ReactNode, onClose: () => void, defaultLayout?: boolean }) {
     if (!defaultLayout)
-        return (<div onClick={onClose} className="fixed inset-0 z-30" >{children}</div>);
+        return (<div onClick={onClose} className="fixed inset-0 z-50" >{children}</div>);
     return (
-        <div onClick={onClose} className="fixed inset-0 flex justify-center items-center z-30 bg-black/50">
+        <div onClick={onClose} className="fixed inset-0 flex justify-center items-center z-50 bg-black/50">
             <div onClick={(e) => e.stopPropagation()} className="p-6 bg-white custom-shadow-m border w-90">
                 <div className="flex flex-col gap-2 items-start">
                     {children}

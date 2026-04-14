060affbffc0410a086df038d4fdaffdb3e64e26e
florian <florian.guiramand@protonmail.com>
2026-04-14 11:02:51 +0200

    style: add default cursor-pointer to Link and button hover

--- frontend/src/components/GenreTag.tsx
+++ frontend/src/components/GenreTag.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function GenreTag( { children }: { children: React.ReactNode; }) {
     return (<button
-        className="px-3 custom-condensed hover:cursor-pointer border text-2xl custom-btn">
+        className="px-3 custom-condensed border text-2xl custom-btn">
         {children}</button>);
 }
--- frontend/src/components/MovieCard.tsx
+++ frontend/src/components/MovieCard.tsx
@@ -13,7 +13,7 @@ export default function MovieCard({movie} : {movie: tmovie}) {
     });
 
     return (
-        <Link href={"/movies/" + movie.id} className="relative aspect-824/560 overflow-hidden cursor-pointer group border">
+        <Link href={"/movies/" + movie.id} className="relative aspect-824/560 overflow-hidden group border">
             <Image className="size-full object-cover transition-transform duration-200 group-hover:scale-103" width={1000} height={1000} src={"/images/" + randomBackdrop} alt={"poster of movie: " + movie.title}/>
             <div className="absolute inset-0 p-4 flex items-end">
                 <div className="bg-gradient"></div>

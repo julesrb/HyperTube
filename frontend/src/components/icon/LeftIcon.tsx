fa54a5f711b318714f16bd9fe6e8619474d15e3a
florian <florian.guiramand@protonmail.com>
2026-04-14 13:51:30 +0200

    refactor: move all icon in one Icon.tsx file

--- frontend/src/components/icon/LeftIcon.tsx
+++
@@ -1,8 +0,0 @@
-export default function LeftIcon({ color = "black", size = 13 }) {
-    const fullColor = `var(--color-${color})`;
-
-    return (<svg width={size} height={size} viewBox="0 0 16 27" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M13.2107 1.34854L2.71069 12.8485L13.7107 24.8485" stroke={fullColor} stroke-width="4"/>
-        </svg>
-    );
-}
--- frontend/src/components/icon/RightIcon.tsx
+++
@@ -1,8 +0,0 @@
-export default function RightIcon({ color = "black", size = 13 }) {
-    const fullColor = `var(--color-${color})`;
-
-    return (<svg width={size} height={size} viewBox="0 0 16 27" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1.97437 24.8514L12.4744 13.3514L1.47437 1.35144" stroke={fullColor} stroke-width="4"/>
-        </svg>
-    );
-}

6ef8fd43fadc809a3277a281fe429d0fef54465d
florian <florian.guiramand@protonmail.com>
2026-04-15 10:05:20 +0200

    refactor: clean GenreTag.tsx file

--- frontend/src/components/GenreTag.tsx
+++ frontend/src/components/GenreTag.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-export default function GenreTag( { children }: { children: React.ReactNode; }) {
-    return (<button
-        className="px-3 custom-condensed border text-2xl custom-btn">
-        {children}</button>);
+export default function GenreTag({children}: { children: React.ReactNode; }) {
+    return (<button className="px-3 custom-condensed border text-2xl custom-btn">
+        {children}
+    </button>);
 }

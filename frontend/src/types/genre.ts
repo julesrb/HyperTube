998d4473446628503d56273da1fe3038132e94e3
florian <florian.guiramand@protonmail.com>
2026-04-15 14:06:32 +0200

    add: create genre.ts type and data

---
+++ frontend/src/types/genre.ts
@@ -0,0 +1,47 @@
+export type tgenre = {
+    id: number
+    name: string
+};
+
+export const genres: tgenre[] = [
+    {
+        id: 0,
+        name: "Animation",
+    },
+    {
+        id: 1,
+        name: "Aventure",
+    },
+    {
+        id: 2,
+        name: "Comédie",
+    },
+    {
+        id: 3,
+        name: "Drame",
+    },
+    {
+        id: 4,
+        name: "Familial",
+    },
+    {
+        id: 5,
+        name: "Fantastique",
+    },
+    {
+        id: 6,
+        name: "Horreur",
+    },
+    {
+        id: 7,
+        name: "Romance",
+    },
+    {
+        id: 8,
+        name: "Science-Fiction",
+    },
+    {
+        id: 9,
+        name: "Thriller",
+    }
+];

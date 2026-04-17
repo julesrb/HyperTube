8bdb8f8d6738d56b0a20ed5620a931e89250a122
florian <florian.guiramand@protonmail.com>
2026-04-18 01:36:14 +0200

    update: update <Input> component my handle id, value and setValue

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {EyeIcon} from "@/components/Icon";
 
-export default function Input({type, placeholder, className }: {type: string, placeholder: string, className?: string}) {
+export default function Input({id, type, placeholder, value, onChange, className }: {id: string, type: string, placeholder: string, value: string, onChange: (e: React.ChangeEvent<HTMLInputElement>, key: string) => void, className?: string}) {
     const isPassword = type === "password";
     const [isPasswordVisible, setIsPasswordVisible] = React.useState(false);
 
@@ -10,10 +10,12 @@ export default function Input({type, placeholder, className }: {type: string, pl
     }
 
     return (<div className={"flex flex-col w-full h-16 relative " + className}>
-        <input id={type} type={isPasswordVisible && isPassword ? "text" : type} placeholder=""
+        <input id={id} type={isPasswordVisible && isPassword ? "text" : type} placeholder=""
+               value={value}
+               onChange={(e) => onChange(e, id)}
                className="peer py-4 m-0 w-full h-8 bg-white text-black border-b focus:border-b-2"/>
         <label htmlFor={type}
-               className="uppercase absolute text-xs font-sans bottom-15
+               className="pointer-events-none uppercase absolute text-xs font-sans bottom-15
                peer-focus:text-xs peer-focus:font-sans peer-focus:bottom-15
                peer-placeholder-shown:font-condensed peer-placeholder-shown:tracking-wide peer-placeholder-shown:bottom-9 peer-placeholder-shown:text-2xl">{placeholder}</label>
         {isPassword && (<button className="absolute right-0 top-1" onClick={handleTogglePasswordVisibility}><EyeIcon crossed={isPasswordVisible}/></button>)}

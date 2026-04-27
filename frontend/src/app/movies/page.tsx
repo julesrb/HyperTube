87971ef001924c10253e39c1067b4f123478c278
florian <florian.guiramand@protonmail.com>
2026-04-27 17:45:12 +0200

    update: focus input field in SearchBar component on mount

--- frontend/src/app/movies/page.tsx
+++ frontend/src/app/movies/page.tsx
@@ -2,7 +2,7 @@
 
 import {movies} from "@/types/movie";
 import {ListMovieCard, MoviesCard} from "@/components/MovieCard";
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {GridIcon, ListIcon} from "@/components/Icons";
 import {CloseButton} from "../../components/Buttons";
 import {useModal} from "@/context/ModalContext";
@@ -44,8 +44,15 @@ export default function Page() {
 }
 
 function SearchBar({searchValue, onChange}: {searchValue: string, onChange: (e?: React.ChangeEvent<HTMLInputElement>) => void}) {
+    const inputRef = useRef<HTMLInputElement>(null);
+    useEffect(() => {
+        const el = inputRef.current;
+        if (!el) return;
+        el.focus();
+    }, []);
+
     return (<div className="flex items-center px-6">
-        <input type="search" placeholder="Rechercher un film..." value={searchValue} onChange={onChange}
+        <input ref={inputRef} type="search" placeholder="Rechercher un film..." value={searchValue} onChange={onChange}
         className="w-full bg-white text-8xl font-condensed uppercase border-b focus:border-b-2"></input>
         <CloseButton className="absolute right-10" onClick={() => onChange()} disabled={searchValue.length === 0}/>
     </div>);

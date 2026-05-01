42a46b637dc54da4d6f13d6981a20706634521c9
florian <florian.guiramand@protonmail.com>
2026-05-01 13:32:12 +0200

    update: handle responsive and improve link behavior for mobile in MovieHero component

--- frontend/src/components/MovieHero.tsx
+++ frontend/src/components/MovieHero.tsx
@@ -35,21 +35,24 @@ function MovieHero({movie, onClick, onClickLeft, onClickRight, backdrop}: { movi
         randomBackdrop = backdrop;
 
     return (<div className="px-4 min-w-full">
-        <div className="relative flex flex-col items-center gap-4 aspect-21/9 border">
+        <div className="relative flex flex-col items-center gap-4 aspect-16/9 xl:aspect-21/9 border">
             <Image className="size-full object-cover" width={5000} height={5000} loading="eager"
                    src={"/images/" + randomBackdrop} alt={"poster of movie " + movie.title}/>
             <div className="h-full w-50 z-30 absolute left-0 custom-cursor-left"
                  onClick={onClickLeft}></div>
-            {onClick && <div className="h-full w-full z-20 absolute custom-cursor-play" onClick={onClick}></div>}
+            {onClick ?
+                <div className="h-full w-full z-20 absolute custom-cursor-play" onClick={onClick}></div>
+                : <Link href={"/movies/" + movie.id} className="h-full w-full z-20 absolute"></Link>
+            }
             <div className="h-full w-50 z-30 absolute right-0 custom-cursor-right"
                  onClick={onClickRight}></div>
             <div className="absolute inset-0 text-white flex items-end justify-center text-center mx-auto">
                 <div className="bg-gradient"></div>
-                <Link href={"/movies/" + movie.id} className="z-40 max-w-2/3 p-6">
+                <Link href={"/movies/" + movie.id} className="absolute z-40 max-w-2/3 bottom-1/20">
                     {
                         backdrop === undefined ?
                         <h1 className="relative hover:underline decoration-3 underline-offset-3">{movie.title}
-                            <span className="absolute -right-18 font-hairline text-3xl tracking-normal">{movie.year}</span>
+                            <span className="absolute -right-8 sm:-right-13 xl:-right-18 responsive-text-hairline">{movie.year}</span>
                         </h1> :
                         <SecondaryButton className="my-4 font-bold text-xl h-12" onClick={() => console.log("watch movie")}>Watch</SecondaryButton>
                     }

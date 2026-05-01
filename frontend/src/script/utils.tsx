d9b274762dd2c19f25beb9b1e777d5daf1fa49ca
florian <florian.guiramand@protonmail.com>
2026-05-01 13:29:13 +0200

    add: add responsive size hook for dynamic layout adjustments

--- frontend/src/script/utils.tsx
+++ frontend/src/script/utils.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {tMovie} from "@/types/movie";
 
 export function useRandomBackdrop(movie: tMovie) {
@@ -8,4 +8,26 @@ export function useRandomBackdrop(movie: tMovie) {
     });
 
     return (randomBackdrop);
+}
+
+
+type tSize = "xs" | "md" | "xl";
+
+export function useResponsiveSize() {
+    const [size, setSize] = useState<tSize>("xl");
+
+    useEffect(() => {
+        function handleResize() {
+            if (window.innerWidth >= 1024)
+                setSize("xl");
+            else if (window.innerWidth >= 768)
+                setSize("md");
+            else
+                setSize("xs");
+        }
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+    return size;
 }

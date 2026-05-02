f0259ffdb38b1ccbc58faacc30c3e76584791d49
florian <florian.guiramand@protonmail.com>
2026-05-02 17:13:32 +0200

    update: handle responsive in pagination component and add conditional rendering

--- frontend/src/components/Pagination.tsx
+++ frontend/src/components/Pagination.tsx
@@ -8,34 +8,33 @@ export default function Pagination({children, currenIndex, totalPage, onClick} :
     const handleLeftArrow = () => {
         const index = currenIndex - 1;
 
-        if (index >= 0) {
+        if (index >= 0)
             onClick(index);
-        }
     }
 
     const handleRightArrow = () => {
         const index = currenIndex + 1;
 
-        if (index < totalPage) {
+        if (index < totalPage)
             onClick(index);
-        }
     }
 
-    return (<div>
+    return (<div className="w-full">
         {children}
-        <div className="flex w-full gap-2 justify-center my-4">
-            <button className="mt-1" onClick={handleLeftArrow} onMouseEnter={() => setLeftColor(currenIndex === 0 ? "gray" : "black")} onMouseLeave={() => setLeftColor("gray")}>
-                <LeftIcon color={leftColor}/>
-            </button>
-            {Array.from({ length: totalPage }, (_, i) => (
-                <button key={i} className={"custom-condensed text-2xl leading-6 " + (i === currenIndex ? "text-black font-bold" : "text-gray hover:underline")} onClick={() => {onClick(i)}}>
-                    {i}
+        { totalPage !== 0 &&
+            <div className="flex w-full gap-2 justify-center my-4">
+                <button className="mt-1" onClick={handleLeftArrow} onMouseEnter={() => setLeftColor(currenIndex === 0 ? "gray" : "black")} onMouseLeave={() => setLeftColor("gray")}>
+                    <LeftIcon color={leftColor}/>
                 </button>
-            ))}
+                {Array.from({ length: totalPage }, (_, i) => (
+                    <button key={i} className={"custom-condensed text-2xl leading-6 " + (i === currenIndex ? "text-black font-bold" : "text-gray hover:underline")} onClick={() => {onClick(i)}}>
+                        {i}
+                    </button>
+                ))}
 
-            <button className="mt-1" onClick={handleRightArrow} onMouseEnter={() => setRightColor(currenIndex + 1 === totalPage ? "gray" : "black")} onMouseLeave={() => setRightColor("gray")}>
-                <RightIcon color={rightColor}/>
-            </button>
-        </div>
+                <button className="mt-1" onClick={handleRightArrow} onMouseEnter={() => setRightColor(currenIndex + 1 === totalPage ? "gray" : "black")} onMouseLeave={() => setRightColor("gray")}>
+                    <RightIcon color={rightColor}/>
+                </button>
+        </div>}
     </div>);
 }

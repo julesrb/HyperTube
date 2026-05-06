04eae0ed2048130de45257ec04c0600b59fb579a
florian <florian.guiramand@protonmail.com>
2026-05-06 16:32:55 +0200

    fix: handle movie not found case in MoviePage component

--- frontend/src/app/[locale]/movies/[id]/page.tsx
+++ frontend/src/app/[locale]/movies/[id]/page.tsx
@@ -1,19 +1,18 @@
 "use client";
 
-import {useParams} from "next/navigation";
+import {notFound, useParams} from "next/navigation";
 import {movies} from "@/types/movie";
 import React from "react";
 import {CommentSection} from "@/components/Comments";
 import MovieInfoSection from "@/app/[locale]/movies/[id]/MovieInfoSection";
 import MoviesHero from "@/components/MovieHero";
-import {useTranslations} from "next-intl";
 
 export default function MoviePage() {
     const params = useParams();
     const movie = movies.find((movie) => movie.id === Number(params.id));
-    const t = useTranslations("movie");
 
-    if (!movie) return <p>{t("notFound")}</p>; // todo
+    if (!movie)
+        notFound();
 
     return (<div className="flex flex-col gap-4 sm:gap-6 xl:gap-10">
         <MoviesHero movie={movie} items={movie.backdrops} onClick={() => console.log('play movie')} />

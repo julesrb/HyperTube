4c9f1de0f4055bac8530aefca429bb421c88e13f
florian <florian.guiramand@protonmail.com>
2026-05-06 16:20:14 +0200

    update: refactor LanguageDropdown for improved locale handling

--- frontend/src/components/nav/LanguageDropdown.tsx
+++ frontend/src/components/nav/LanguageDropdown.tsx
@@ -1,19 +1,21 @@
 import React, {useState} from "react";
 import {useRouter, usePathname} from '@/i18n/navigation';
 import {routing, tLocale} from "@/i18n/request";
+import {useLocale} from "next-intl";
+import {useSearchParams} from "next/navigation";
 
 export default function LanguageDropdown(Icon: ({selected}: {selected: boolean}) => React.JSX.Element) {
     const [isOpen, setIsOpen] = useState(false);
     const languages: Record<tLocale, string> = {en: "English", fr: "Français", de: "Deutsch"};
-    const [selectedLanguage, setSelectedLanguage] = useState<Record<tLocale, boolean>>({en: false, fr: false, de: false});
+    const locale = useLocale() as tLocale;
     const router = useRouter();
     const pathname = usePathname();
+    const searchParams = useSearchParams();
 
     const handleSwitchLanguage = (key: tLocale) => {
-        const newLanguage = {en: false, fr: false, de: false};
-        newLanguage[key] = true;
-        setSelectedLanguage(newLanguage);
-        router.replace(pathname, {locale: key});
+        const query = searchParams.toString();
+        const href = query.length > 0 ? `${pathname}?${query}` : pathname;
+        router.replace(href, {locale: key});
         setIsOpen(false);
     }
 
@@ -26,7 +28,7 @@ export default function LanguageDropdown(Icon: ({selected}: {selected: boolean})
         {isOpen && <div className="absolute top-10 right-1/30 z-50 p-8">
             <div className="flex flex-col gap-1 items-start bg-white py-4 px-5 custom-shadow-s border">
                 {routing.locales.map((code) => (
-                    <button key={code} className={"custom-underline text-xl " + (selectedLanguage[code] ? "font-base font-light" : "font-hairline")} onClick={() => handleSwitchLanguage(code)}>{languages[code]}</button>
+                    <button key={code} className={"custom-underline text-xl " + (locale === code ? "font-base font-light" : "font-hairline")} onClick={() => handleSwitchLanguage(code)}>{languages[code]}</button>
                 ))}
             </div>
         </div>}
